3460952b6c0492882b7b32bfd89e90a803991c92
Klayvert <klayvert.ryan16@gmail.com>
2023-05-01 22:50:07 -0300

    Adicionando tratativas para view de visualização e cadastro das tarefas, além de criar novas classes para exclusão e edição dos dados

--- todo_list/core/views.py
+++ todo_list/core/views.py
@@ -2,6 +2,7 @@ from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from core.models import ToDoList
+from datetime import date
 
 class CadastroView(View):
     def get(self, request):
@@ -10,30 +11,37 @@ class CadastroView(View):
         nome_tarefa = request.POST.get('nome_tarefa')
         data_tarefa = request.POST.get('data_hora_add')
         descricao = request.POST.get('descricao_task')
+        if not nome_tarefa or not data_tarefa or not descricao:
+            return HttpResponse('Preencha todos os campos')
         dados = ToDoList.objects.create(nome_tarefa=nome_tarefa, data_tarefa=data_tarefa, descricao=descricao)
         return redirect('todo_list')
 
 class TaskList(View):
     def get(self, request):
         dados = ToDoList.objects.all()
+        if not dados:
+            return HttpResponse('Sem Tarefas')
         return render(request, 'todo_list.html', {'dados': dados})
-        
-    def editar_objeto(request, pk):
-        objeto = get_object_or_404(TaskList, pk=pk)
-        if request.method == 'POST':
-            form = TaskList(request.POST, instance=objeto)
-            if form.is_valid():
-                form.save()
-                return redirect('todo_list')
-        else:
-            form = TaskList(instance=objeto)
-        return render(request, 'editar-objeto.html', {'form': form})
 
-    def deletar_objeto(request, pk):
-        objeto = get_object_or_404(TaskList, pk=pk)
-        if request.method == 'POST':
-            objeto.delete()
-            return redirect('todo_list')
-        return render(request, 'deletar-objeto.html', {'objeto': objeto})
+class EditarView(View):
+    def get(self, request, id):
+        dado = get_object_or_404(ToDoList, id=id)
+        return render(request, 'editar.html', {'dado': dado})
+
+    def post(self, request, id):
+        dado = get_object_or_404(ToDoList, id=id)
+        dado.nome_tarefa = request.POST.get('nome_tarefa')
+        dado.data_tarefa = request.POST.get('data_hora_add')
+        dado.descricao = request.POST.get('descricao_task')
+        dado.save()
+        return redirect('todo_list')
     
-        
+class DeletarView(View):
+    def get(self, request, id):
+        dado = get_object_or_404(ToDoList, id=id)
+        return render(request, 'deletar.html', {'dado': dado})
+
+    def post(self, request, id):
+        dado = get_object_or_404(ToDoList, id=id)
+        dado.delete()
+        return redirect('todo_list')
